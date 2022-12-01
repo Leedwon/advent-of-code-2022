b90d475fa5a23f7076e6964dec5253403634f2cc
Jakub Ledwon <leedwon12@gmail.com>
2022-12-01 16:27:19 +0100

    solve day1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
-fun main() {
+import day1.solveDay11
+import day1.solveDay12
 
+fun main() {
+    solveDay11()
+    solveDay12()
 }
---
+++ src/main/kotlin/day1/CaloriesCounter.kt
@@ -0,0 +1,37 @@
+package day1
+
+import util.readFileLines
+
+private const val inputFile = "/day_1_1.txt"
+
+fun solveDay11() {
+    val calories = sumElvesCalories(inputFile)
+    println(calories)
+}
+
+fun solveDay12() {
+    val calories = sumElvesCalories(inputFileName = inputFile, elvesNumber = 3)
+    println(calories)
+}
+
+/**
+ * @param elvesNumber - number of elves with the highest amount of calories. Their calories should be counted.
+ * @return sum of calories of elves with most calories. [elvesNumber] of elves will be taken into account.
+ */
+private fun sumElvesCalories(inputFileName: String, elvesNumber: Int = 1): Int {
+    val elves = parseInput(readFileLines(inputFileName))
+    return elves.sortedByDescending { it.calories }.take(elvesNumber).sumOf { it.calories }
+}
+
+private fun parseInput(lines: List<String>): List<Elf> =
+    lines.fold(initial = (emptyList<Elf>() to 0)) { acc, line ->
+        val (elves, runningCalories) = acc
+
+        if (line.isEmpty()) {
+            elves + Elf(runningCalories) to 0
+        } else {
+            elves to runningCalories + line.toInt()
+        }
+    }.first
+
+private data class Elf(val calories: Int)
